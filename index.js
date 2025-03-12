fb3995b2396dee0f65f344a66ee4940e318c63bc
Wesley Oliveira <wesleybrasil1997@gmail.com>
2025-03-12 08:10:55 -0300

    Ajustes js

--- index.js
+++ index.js
@@ -19,13 +19,41 @@ botaoAdicionar.addEventListener("click", (evento) => {
     const inputCheckbox = document.createElement("input");
     inputCheckbox.type = "checkbox";  
     inputCheckbox.id = "checkbox-" + contador++;
-    
+
     const nomeItem = document.createElement("p");
     nomeItem.innerText = inputItem.value;
+
+    inputCheckbox.addEventListener("click", function() {
+        if (inputCheckbox.checked) {
+            nomeItem.style.textDecoration = "line-through"
+        } else {
+            nomeItem.style.textDecoration = "none"
+        }
+    })
+    
+
 
     containerItemDalista.appendChild(inputCheckbox);
     containerItemDalista.appendChild(nomeItem);
     itemDaLista.appendChild(containerItemDalista);
+
+    const diaDaSemana = new Date().toLocaleDateString("pt-BR", {weekday: "long"});
+    const data = new Date().toLocaleDateString("pt-BR");
+    const hora = new Date().toLocaleTimeString("pt-BR", {
+        hour: "numeric",
+        minute: "numeric"
+    })
+
+    const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`
+    const itemData = document.createElement("p");
+    itemData.classList.add("texto-data");
+
+    itemData.innerText = dataCompleta;
+    itemDaLista.appendChild(itemData);
+
     listaDeCompras.appendChild(itemDaLista);
+
+
+  
 })
 
