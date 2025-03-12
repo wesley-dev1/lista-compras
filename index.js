47a465832ed64901794bea9fa7c03de43ef50124
Wesley Oliveira <wesleybrasil1997@gmail.com>
2025-03-12 06:59:32 -0300

    Alteracao js

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 const inputItem = document.getElementById("input-item")
+const listaDeCompras = document.getElementById("lista-de-compras")
+const botaoAdicionar = document.getElementById("adicionar-item")
 
-console.log(inputItem.value);
+let contador = 0;
+
+botaoAdicionar.addEventListener("click", (evento) => {
+    evento.preventDefault();
+    if (inputItem.value === ""){
+        alert("Por favor, insira um item!");
+        return
+    }
+
+    const itemDaLista = document.createElement("li");
+
+    const containerItemDalista = document.createElement("div");
+    containerItemDalista.classList.add("lista-item-container");
+
+    const inputCheckbox = document.createElement("input");
+    inputCheckbox.type = "checkbox";  
+    inputCheckbox.id = "checkbox-" + contador++;
+    
+    const nomeItem = document.createElement("p");
+    nomeItem.innerText = inputItem.value;
+
+    containerItemDalista.appendChild(inputCheckbox);
+    containerItemDalista.appendChild(nomeItem);
+    itemDaLista.appendChild(containerItemDalista);
+    listaDeCompras.appendChild(itemDaLista);
+})
+
